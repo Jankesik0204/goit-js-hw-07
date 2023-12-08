2a8ac72c788664d25fca8751edbb6accf8ef3608
Jankesik0204 <135467289+Jankesik0204@users.noreply.github.com>
2023-12-08 13:50:36 +0100

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,13 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+// 1.Utworzy oddzielny element <li>. Pamiętaj, aby użyć metody document.createElement().
+// 2.Doda nazwę składnika jako jego zawartość tekstową.
+// 3.Doda klasę item do elementu.
+// 4.Wstawi wszystkie <li> w jednej operacji do listy ul#ingredients.
