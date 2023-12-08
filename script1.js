5858f33f7e3eade8f35c89c6fd147c1b8da5c138
Jankesik0204 <135467289+Jankesik0204@users.noreply.github.com>
2023-12-08 13:47:23 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,5 +1,23 @@
-const ULs= document.getElementById("categories");
-console.log(ULs)
-const LIs= document.getElementById(".item");
-console.log(LIs)
+// const ulByFirstChild = document.body.firstElementChild;
+// console.log("Pierwsze dziecko body:", ulByFirstChild);
+// const ul = document.getElementById("categories");
+// console.log(
+//   "Wyswietlenie wszystkich elementow znajdujacych sie w naszym UL:  ",
+//   ul
+// );
+const LIs = document.getElementsByClassName("item");
+console.log("Tablica elementow z klasa item: ", LIs);
+//1.Policzy i wyświetli w konsoli liczbę kategorii w ul#categories, czyli elementów li.item.
+// const amount_of_LI1 = LIs.length;
+// console.log("Number of categories: ", amount_of_LI1);
+const allCategories = document.querySelectorAll(".item");
+const amount_of_LI2 = allCategories.length;
+console.log("Number of categories: ", amount_of_LI2);
+
+for (const category of allCategories) {
+  console.log("Kategoria:", category.querySelector("h2").textContent);
+  const c1 = category.querySelector("ul");
+  const c2 = c1.querySelectorAll("li").length;
+  console.log("Elemnets: ", c2);
+}
 
